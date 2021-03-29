410895038bac04dfdb9ec67416f9e6753b1227fb
adriansauer <adriansauer1999@gmail.com>
2021-03-29 17:36:34 -0400

    ASO-24 implementacion de la pantalla de login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './App.css'
+import 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/js/materialize.min.js'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Login from './components/Login'
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,12 +1,55 @@
 import React from 'react'
-import image from '../images/construccion.png'
+
 const Login = () => {
   return (
-  <div>
-        <h1 aria-setsize={52}>
-            Página de login
-       </h1>
-        <img src={image} alt="Logo" />
-    </div>)
+    <div className="container">
+      <div className="row">
+        <div className="col m6 offset-m3">
+          <h2 className="center-align">Login</h2>
+          <div className="row">
+            {/** error && (
+              <ErrorNotice
+                message={error}
+                clearError={() => setError(undefined)}
+              />
+            ) */}
+            <form>
+              <div className="row">
+                <div className="input-field col s12">
+                  <label><i className="material-icons">perm_contact_calendar
+</i>Código: </label>
+                  <input
+                    type="text"
+                    id="codigo"
+                  />
+                </div>
+              </div>
+              <div className="row">
+                <div className="input-field col s12">
+                <label><i className="material-icons">security
+</i>Contraseña: </label>
+                  <input type="password" id="password" />
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col m12">
+                  <p className="center-align">
+                    <button
+                      className="btn btn-large waves-effect waves-light"
+                      type="submit"
+                      name="action"
+                    >
+                      Login
+                    </button>
+                  </p>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 export default Login
