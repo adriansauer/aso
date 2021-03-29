91826831ea9f910923524db3cc1dbc78629d0dca
adriansauer <adriansauer1999@gmail.com>
2021-03-29 18:12:42 -0400

    ASO-30 implementacion del header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,13 @@ import 'materialize-css/dist/js/materialize.min.js'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Login from './components/Login'
+import Header from './components/Header'
+
 function App () {
   return (
     <div className="App">
        <Router>
+        <Header/>
         <>
           <Switch>
           <Route exact path="/" component={Home} />
