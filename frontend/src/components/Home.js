c688d891468f368197d7df926ba17e4a6e6a671f
adriansauer <adriansauer1999@gmail.com>
2021-03-29 16:05:46 -0400

    ASO-23 configuro enrutamiento, y creo los archivos Login.js y Home.js para prueba de enrutamiento

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import image from '../images/construccion.png'
+const Home = () => {
+  return (
+  <div>
+        <h1 aria-setsize={52}>
+            Página de inicio
+       </h1>
+        <img src={image} alt="Logo" />
+    </div>)
+}
+export default Home
