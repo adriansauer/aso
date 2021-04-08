f91aabce70e6fd07ab67bb52f4ab7cb634c43081
adriansauer <adriansauer1999@gmail.com>
2021-04-08 08:20:03 -0400

    ASO-39 agrego peticion de axios que permite eliminar un usuario existente mediante el Id de dicho usuario

---
+++ frontend/src/api/user/useDeleteUser.js
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import api from '../api'
+
+const useDeleteUser = () => {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  const execute = id => {
+    setError(null)
+    setLoading(true)
+    api.delete(`/users/${id}`)
+      .then(response => {
+        setData(response.data)
+        setError(null)
+        setLoading(false)
+      })
+      .catch(_error => {
+        setData(null)
+        setError('Ha ocurrido un error al intentar eliminar el usuario')
+        setLoading(false)
+      })
+  }
+
+  return { data, loading, error, execute }
+}
+
+export default useDeleteUser
