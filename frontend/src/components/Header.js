005ea2f280c4d7e8004fdbdd70f7451a93cb1e49
adriansauer <adriansauer1999@gmail.com>
2021-04-13 06:30:51 -0400

    ASO-26 creo la opcion de logout en el heacer

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import M from 'materialize-css/dist/js/materialize.min.js'
 import perfil from '../images/perfil.jpg'
 import './components.css'
+import userContext from '../context/userContext'
 const Header = () => {
   const [instance, setInstance] = useState(null)
+  const { setUserData, isAutenticate } = useContext(userContext)
   /** Cuando se levanta el componente se instancia el Sidebar para almacenarlo en el estado y manipularlo desde ahi */
   useEffect(() => {
     const elem = document.querySelector('.sidenav')
@@ -24,40 +26,63 @@ const Header = () => {
       }
     }
   }
-
+  const handleLogout = () => {
+    setUserData({
+      token: null,
+      user: {
+        displayName: null,
+        id: null,
+        roles: null
+      }
+    })
+  }
   return (
     <>
-    {/** Vista del navbar */}
+      {/** Vista del navbar */}
       <nav>
         <div className="nav-wrapper" style={{ backgroundColor: '#0C0019' }}>
-          <a href="#" className="brand-logo right">
-            <ul>
-              <li>
-                <h6 style={{ marginRight: '2%' }}>Victor Gimenez</h6>
-              </li>
-              <li>
-                <i className="material-icons" style={{ marginLeft: '2%' }}>
-                  exit_to_app
-                </i>
-              </li>
-              <li>
-                <img className="circle" width={60} height={60} src={perfil} />
-              </li>
-            </ul>
-          </a>
+          {isAutenticate
+            ? (
+            <>
+              <a href="#" className="brand-logo right">
+                <ul>
+                  <li>
+                    <h6 style={{ marginRight: '2%' }}>Victor Gimenez</h6>
+                  </li>
+                  <li>
+                    <i
+                      className="material-icons"
+                      onClick={handleLogout}
+                      style={{ marginLeft: '2%' }}
+                    >
+                      exit_to_app
+                    </i>
+                  </li>
+                  <li>
+                    <img
+                      className="circle"
+                      width={60}
+                      height={60}
+                      src={perfil}
+                    />
+                  </li>
+                </ul>
+              </a>
 
-            <ul id="nav-mobile" className="left">
-              <li>
-                <button
-                  style={{ backgroundColor: '#0C0019' }}
-                  className="btn btn-large waves-effect waves-light"
-                  onClick={handleToggle}
-                >
-                  <i className="material-icons">dehaze</i>
-                </button>
-              </li>
-            </ul>
-
+              <ul id="nav-mobile" className="left">
+                <li>
+                  <button
+                    style={{ backgroundColor: '#0C0019' }}
+                    className="btn btn-large waves-effect waves-light"
+                    onClick={handleToggle}
+                  >
+                    <i className="material-icons">dehaze</i>
+                  </button>
+                </li>
+              </ul>
+            </>
+              )
+            : null}
         </div>
       </nav>
       {/** Vista del Sidebar */}
