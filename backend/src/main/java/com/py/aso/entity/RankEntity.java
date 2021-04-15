6856ad5935edb6574a92cf27ee18491c4d9cb337
VictorCiceia <victormciceia@gmail.com>
2021-04-15 14:18:51 -0400

    ASO-46 Creacion de objeto de ranges

--- backend/src/main/java/com/py/aso/entity/RankEntity.java
+++ backend/src/main/java/com/py/aso/entity/RankEntity.java
@@ -1,34 +1,38 @@
 package com.py.aso.entity;
 
-import java.util.Set;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
 
 @Entity
 @Data
-@Table(name = "images")
-public class ImageEntity {
+@Table(name = "ranges")
+public class RankEntity {
 
 	@Id
 	@Column
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(name = "name")
-	private String name;
+	@Column(name = "title")
+	private String title;
 
-	@Column(name = "path")
-	private String path;
+	@Column(name = "description")
+	private String description;
 
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "image")
-	private Set<RankEntity> ranges;
+	@Column(name = "deleted")
+	private boolean deleted;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "image_id")
+	private ImageEntity image;
+
 }
