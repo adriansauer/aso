47aa0fc5118ec30da75e4c8bb31fc304015e548b
VictorCiceia <victormciceia@gmail.com>
2021-04-16 17:53:52 -0400

    ASO-50 Creacion del mapper de ranges

--- backend/src/main/java/com/py/aso/dto/create/RankCreateDTO.java
+++ backend/src/main/java/com/py/aso/dto/create/RankCreateDTO.java
@@ -1,4 +1,4 @@
-package com.py.aso.dto;
+package com.py.aso.dto.create;
 
 import javax.validation.constraints.NotBlank;
 
@@ -8,15 +8,16 @@ import lombok.Data;
 
 @Data
 @ApiModel(description = "Objeto que representa a un un rango de los bomberos")
-public class RankDTO {
-
-	private Long id;
+public class RankCreateDTO {
 
 	@NotBlank(message = "El nombre del rango es requerido")
 	@ApiModelProperty(notes = "title", example = "Comandante", required = true)
 	private String title;
 
-	@ApiModelProperty(notes = "imageId", example = "1", required = true)
+	@ApiModelProperty(notes = "description", example = "Es el rango mas alto")
+	private String description;
+
+	@ApiModelProperty(notes = "imageId", example = "1")
 	private long imageId;
 
 }
--- backend/src/main/java/com/py/aso/dto/detail/RankDetailDTO.java
+++ backend/src/main/java/com/py/aso/dto/detail/RankDetailDTO.java
@@ -1,4 +1,4 @@
-package com.py.aso.dto;
+package com.py.aso.dto.detail;
 
 import javax.validation.constraints.NotBlank;
 
@@ -8,13 +8,16 @@ import lombok.Data;
 
 @Data
 @ApiModel(description = "Objeto que representa a un un rango de los bomberos")
-public class RankDTO {
+public class RankDetailDTO {
 
 	private Long id;
 
 	@NotBlank(message = "El nombre del rango es requerido")
 	@ApiModelProperty(notes = "title", example = "Comandante", required = true)
 	private String title;
+
+	@ApiModelProperty(notes = "description", example = "Es el rango mas alto")
+	private String description;
 
 	@ApiModelProperty(notes = "imageId", example = "1", required = true)
 	private long imageId;
