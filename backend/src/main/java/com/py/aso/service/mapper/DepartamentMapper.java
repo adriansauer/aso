927079a1945c40a2c215192403548e488e7adf7f
VictorCiceia <victormciceia@gmail.com>
2021-04-16 17:27:50 -0400

    ASO-49 Creacion de mapper de departaments

--- backend/src/main/java/com/py/aso/service/mapper/DepartamentMapper.java
+++ backend/src/main/java/com/py/aso/service/mapper/DepartamentMapper.java
@@ -1,5 +1,50 @@
 package com.py.aso.service.mapper;
 
-public class DepartamentMapper {
+import com.py.aso.dto.DepartamentDTO;
+import com.py.aso.dto.create.DepartamentCreateDTO;
+import com.py.aso.dto.detail.DepartamentDetailDTO;
+import com.py.aso.dto.update.DepartamentUpdateDTO;
+import com.py.aso.entity.DepartamentEntity;
+
+public class DepartamentMapper implements
+		BaseMapper<DepartamentEntity, DepartamentDTO, DepartamentDetailDTO, DepartamentCreateDTO, DepartamentUpdateDTO> {
+
+	@Override
+	public DepartamentDTO toDTO(DepartamentEntity entity) {
+		DepartamentDTO dto = new DepartamentDTO();
+		dto.setId(entity.getId());
+		dto.setName(entity.getName());
+		return dto;
+	}
+
+	@Override
+	public DepartamentDetailDTO toDetailDTO(DepartamentEntity entity) {
+		DepartamentDetailDTO dto = new DepartamentDetailDTO();
+		dto.setId(entity.getId());
+		dto.setName(entity.getName());
+		return dto;
+	}
+
+	@Override
+	public DepartamentEntity toEntity(DepartamentDTO dto) {
+		DepartamentEntity entity = new DepartamentEntity();
+		entity.setId(dto.getId());
+		entity.setName(dto.getName());
+		return entity;
+	}
+
+	@Override
+	public DepartamentEntity toCreateEntity(DepartamentCreateDTO dto) {
+		DepartamentEntity entity = new DepartamentEntity();
+		entity.setName(dto.getName());
+		return entity;
+	}
+
+	@Override
+	public DepartamentEntity toUpdateEntity(DepartamentUpdateDTO dto) {
+		DepartamentEntity entity = new DepartamentEntity();
+		entity.setName(dto.getName());
+		return entity;
+	}
 
 }
