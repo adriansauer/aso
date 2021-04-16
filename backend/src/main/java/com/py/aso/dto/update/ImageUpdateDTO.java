3042fe71ae32f5273310182347129f80d5af479a
VictorCiceia <victormciceia@gmail.com>
2021-04-16 17:41:47 -0400

    ASO-50 Creacion de mapper de image

---
+++ backend/src/main/java/com/py/aso/dto/update/ImageUpdateDTO.java
@@ -0,0 +1,16 @@
+package com.py.aso.dto.update;
+
+import javax.validation.constraints.NotBlank;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+@ApiModel(description = "Objeto que representa a una imagen")
+public class ImageUpdateDTO {
+
+	@NotBlank(message = "El nombre de la imagen es requerida")
+	@ApiModelProperty(notes = "name", example = "Perfil de Juan", required = true)
+	private String name;
+}
---
+++ backend/src/main/java/com/py/aso/service/mapper/ImageMapper.java
@@ -0,0 +1,49 @@
+package com.py.aso.service.mapper;
+
+import com.py.aso.dto.ImageDTO;
+import com.py.aso.dto.create.ImageCreateDTO;
+import com.py.aso.dto.detail.ImageDetailDTO;
+import com.py.aso.dto.update.ImageUpdateDTO;
+import com.py.aso.entity.ImageEntity;
+
+public class ImageMapper implements BaseMapper<ImageEntity, ImageDTO, ImageDetailDTO, ImageCreateDTO, ImageUpdateDTO> {
+
+	@Override
+	public ImageDTO toDTO(ImageEntity entity) {
+		ImageDTO dto = new ImageDTO();
+		dto.setId(entity.getId());
+		dto.setName(entity.getName());
+		return dto;
+	}
+
+	@Override
+	public ImageDetailDTO toDetailDTO(ImageEntity entity) {
+		ImageDetailDTO dto = new ImageDetailDTO();
+		dto.setId(entity.getId());
+		dto.setName(entity.getName());
+		return dto;
+	}
+
+	@Override
+	public ImageEntity toEntity(ImageDTO dto) {
+		ImageEntity entity = new ImageEntity();
+		entity.setId(dto.getId());
+		entity.setName(dto.getName());
+		return entity;
+	}
+
+	@Override
+	public ImageEntity toCreateEntity(ImageCreateDTO dto) {
+		ImageEntity entity = new ImageEntity();
+		entity.setName(dto.getName());
+		return entity;
+	}
+
+	@Override
+	public ImageEntity toUpdateEntity(ImageUpdateDTO dto) {
+		ImageEntity entity = new ImageEntity();
+		entity.setName(dto.getName());
+		return entity;
+	}
+
+}
