cf53180b1bac28ff5c2d1de5802dbe1bcebc4e0a
adriansauer <adriansauer1999@gmail.com>
2021-04-24 07:28:53 -0400

    ASO-81 conecto con el backend agregar un miembro a una brigada

--- frontend/src/components/CreateUserForm.js
+++ frontend/src/components/CreateUserForm.js
@@ -1,35 +1,108 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import M from 'materialize-css'
-import useCreateUser from '../api/user/useCreateUser'
-
+import useCreateMember from '../api/miembros/useCreateMember'
+import UseGetDepartaments from '../api/departamento/useGetDepartament'
+import useGetCity from '../api/city/useGetCity'
+import useGetRangos from '../api/rangos/useGetRangos'
 const CreateUserForm = (props) => {
-  const { execute: createUserExecute } = useCreateUser()
-  const [name, setName] = useState('')
+  const { execute: getDepartamentsExecute } = UseGetDepartaments()
+  const { execute: getCityExecute } = useGetCity()
+  const { execute: createMemberExecute } = useCreateMember()
+  const { execute: getRangosExecute } = useGetRangos()
+  // LISTA DE DEPARTAMENTOS
+  const [departaments, setDepartaments] = useState(null)
+  // LISTA DE CIUDADES
+  const [cities, setCities] = useState(null)
+  // LISTA DE RANGOS
+  const [rangos, setRangos] = useState(null)
+  // NOMBRE Y APELLIDO
   const [lastname, setLastname] = useState('')
+  const [name, setName] = useState('')
+  // DIRECCION Y EMAIL
+  const [address, setAddress] = useState('')
   const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  // FECHA DE ADMISION Y CUMPLEANHOS
+  const [admission, setAdmission] = useState(new Date())
+  const [birthday, setBirthday] = useState(new Date())
+  // DEPARTAMENTO Y CIUDAD
+  const [cityId, setCityId] = useState(null)
+  const [departamentId, setDepartamentId] = useState(null)
+  // BRIGADA Y RANGO
+  const [brigadeId, setBrigadaId] = useState('')
+  const [rankId, setRankId] = useState(null)
+  // CELULAR Y CEDULA DE IDENTIDAD
+  const [ci, setCi] = useState('')
+  const [phone, setPhone] = useState('')
+  // UNA DESCRIPCION Y CODIGO DE USUARIO
+  const [description, setDescription] = useState('')
   const [usercode, setUsercode] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
-
+  // CONTRASENHA Y CONFIRMACION DE CONTRASENHA
+  const [password, setPassword] = useState('')
+  const [repeatPassword, setRepeatPassword] = useState('')
+  useEffect(() => {
+    // OBTENGO TODOS LOS DEPARTAMENTOS
+    getDepartamentsExecute()
+      .then((res) => {
+        setDepartaments(res.data.content)
+        M.AutoInit()
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    // OBTENGO TODAS LAS CIUDADES
+    getCityExecute()
+      .then((res) => {
+        setCities(res.data.content)
+        M.AutoInit()
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    // OBTENGO TODOS LOS RANGOS
+    getRangosExecute()
+      .then((res) => {
+        setRangos(res.data.content)
+        M.AutoInit()
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    // OBTENGO LA BRIGADA
+    if (props.brigada !== undefined) {
+      setBrigadaId(props.brigada.id)
+    } else {
+      setBrigadaId(null)
+    }
+  }, [])
   const createUser = (e) => {
     e.preventDefault()
-    if (password !== confirmPassword) {
-      M.toast({ html: 'Las contraseñas no coinciden' })
-    } else {
-      createUserExecute({
-        roles: [{ authority: 'ROLE_USER' }],
-        name,
-        lastname,
-        email,
-        password,
-        usercode
-      }).then((res) => {
+
+    createMemberExecute({
+      address,
+      admission,
+      birthday,
+      brigadeId,
+      ci,
+      cityId,
+      departamentId,
+      description,
+      email,
+      lastname,
+      name,
+      password,
+      phone,
+      rankId,
+      repeatPassword,
+      usercode
+    })
+      .then((res) => {
+        M.toast({ html: 'Miembro agregado exitosamente' })
         props.close()
-      }).catch((err) => {
-        M.toast({ html: err.response.message })
       })
-    }
+      .catch((err) => {
+        console.log(err.response)
+      })
   }
 
   return (
@@ -38,7 +111,7 @@ const CreateUserForm = (props) => {
         <div className="modal-content">
           <h4>Agregue un miembro a la brigada</h4>
 
-          {/** Email y Nombre del usaurio */}
+          {/** NOMBRE Y APELLIDO */}
           <div className="row">
             <div className="input-field col m6">
               <label>Nombre </label>
@@ -61,7 +134,7 @@ const CreateUserForm = (props) => {
               </div>
             </div>
           </div>
-          {/** Apellido y Codigo del usaurio */}
+          {/** EMAIL Y DIRECCION */}
           <div className="row">
             <div className="input-field col m6">
               <label>Email: </label>
@@ -74,11 +147,161 @@ const CreateUserForm = (props) => {
             </div>
             <div className="row">
               <div className="input-field col m6">
-                <label>Código </label>
+                <label>Direccion: </label>
                 <input
                   type="text"
-                  value={usercode}
-                  onChange={(e) => setUsercode(e.target.value)}
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                />
+              </div>
+            </div>
+          </div>
+          {/** FECHA DE NACIMIENTO Y FECHA DE ADMISION */}
+          <div className="row">
+            <div className="input-field col m6">
+              <label>Fecha de nacimiento: </label>
+              <input
+                required
+                type="date"
+                value={birthday}
+                onChange={(e) => setBirthday(e.target.value)}
+              />
+            </div>
+            <div className="input-field col m6">
+              <label>Fecha de admision: </label>
+              <input
+                required
+                type="date"
+                value={admission}
+                onChange={(e) => setAdmission(e.target.value)}
+              />
+            </div>
+          </div>
+          {/** Ciudad y Departamento */}
+          <div className="row">
+            {cities !== null
+              ? (
+              <div className="input-field col s6">
+                <select
+                  defaultValue={'null'}
+                  onChange={(e) => {
+                    setCityId(
+                      e.target.options[e.target.options.selectedIndex].value
+                    )
+                  }}
+                >
+                  <option value={'null'}>Seleccione una ciudad</option>
+                  {cities.map((city) => (
+                    <option key={city.id} value={city.id}>
+                      {city.name}
+                    </option>
+                  ))}
+                </select>
+
+                <label>City</label>
+              </div>
+                )
+              : null}
+
+            {departaments !== null
+              ? (
+              <div className="input-field col s6">
+                <select
+                  defaultValue={'null'}
+                  onChange={(e) => {
+                    setDepartamentId(
+                      e.target.options[e.target.options.selectedIndex].value
+                    )
+                  }}
+                >
+                  <option value={'null'}>Seleccione un departamento</option>
+                  {departaments.map((depto) => (
+                    <option key={depto.id} value={depto.id}>
+                      {depto.name}
+                    </option>
+                  ))}
+                </select>
+
+                <label>Departamento</label>
+              </div>
+                )
+              : null}
+          </div>
+          {/** RANGO Y BRIGADA */}
+          <div className="row">
+            {rangos !== null
+              ? (
+              <div className="input-field col s6">
+                <select
+                  defaultValue={'null'}
+                  onChange={(e) => {
+                    setRankId(
+                      e.target.options[e.target.options.selectedIndex].value
+                    )
+                  }}
+                >
+                  <option value={'null'}>Seleccionar un rango</option>
+                  {rangos.map((rango) => (
+                    <option key={rango.id} value={rango.id}>
+                      {rango.title}
+                    </option>
+                  ))}
+                </select>
+
+                <label>Rango</label>
+              </div>
+                )
+              : null}
+            <div className="input-field col m6">
+              <input
+                disabled
+                type="text"
+                value={props.brigada === undefined ? '' : props.brigada.name}
+              />
+            </div>
+          </div>
+          {/** CEDULA DE IDENTIDAD Y CELULAR */}
+          <div className="row">
+            <div className="input-field col m6">
+              <label>CI: </label>
+              <input
+                required
+                type="number"
+                value={ci}
+                onChange={(e) => setCi(e.target.value)}
+              />
+            </div>
+            <div className="row">
+              <div className="input-field col m6">
+                <label>Celular: </label>
+                <input
+                  required
+                  type="number"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                />
+              </div>
+            </div>
+          </div>
+          {/** USERCODE Y DESCRIPCION */}
+          <div className="row">
+            <div className="input-field col m6">
+              <label>Usercode: </label>
+              <input
+                required
+                type="text"
+                value={usercode}
+                onChange={(e) => setUsercode(e.target.value)}
+              />
+            </div>
+            <div className="row">
+              <div className="input-field col m6">
+                <label>Descripcion: </label>
+                <input
+                  required
+                  type="text"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
                 />
               </div>
             </div>
@@ -96,12 +319,12 @@ const CreateUserForm = (props) => {
               />
             </div>
             <div className="input-field col m6">
+              <label>Confirmar Password: </label>
               <input
                 required
                 type="password"
-                placeholder="Confirm password"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
+                value={repeatPassword}
+                onChange={(e) => setRepeatPassword(e.target.value)}
               />
             </div>
           </div>
@@ -128,6 +351,8 @@ const CreateUserForm = (props) => {
   )
 }
 CreateUserForm.propTypes = {
-  close: PropTypes.func
+  close: PropTypes.func,
+  brigada: PropTypes.object
 }
+
 export default CreateUserForm
