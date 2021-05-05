72e158cca43b383812dc932a22cd351f4569d4fe
Wilson Barrios <wilsonbarrios@gmail.com>
2021-05-04 22:42:58 -0400

    ASO-92 Agregue consulta a base de datos a cada metodo

--- backend/src/main/java/com/py/aso/repository/FileRepository.java
+++ backend/src/main/java/com/py/aso/repository/FileRepository.java
@@ -13,11 +13,15 @@ import com.py.aso.entity.FileEntity;
 @Repository
 public interface FileRepository extends JpaRepository<FileEntity, Long> {
 
+	@Query(value = "SELECT * FROM FILES INNER JOIN PUBLICATIONS ON PUBLICATIONS.file_id = FILES.id WHERE FILES.deleted = ?1 AND PUBLICATIONS.deleted = ?2", nativeQuery = true)
 	public Page<FileEntity> findAllByDeleted(final boolean deletedFile, final boolean deletedPublication, final Pageable pageable);
 
+	@Query(value = "SELECT * FROM FILES INNER JOIN PUBLICATIONS ON PUBLICATIONS.file_id = FILES.id WHERE FILES.publication_id = ?1 AND FILES.deleted = ?2 AND PUBLICATIONS.deleted = ?3", nativeQuery = true)
 	public Page<FileEntity> findAllByPublicationIdAndDeleted(final Long publicatioId, final boolean deletedFile, final boolean deletedPublication, final Pageable pageable);
 	
+	@Query(value = "SELECT * FROM FILES INNER JOIN PUBLICATIONS ON PUBLICATIONS.file_id = FILES.id WHERE FILES.id = ?1 AND FILES.deleted = ?2 AND PUBLICATIONS.deleted = ?3", nativeQuery = true)
 	public Optional<FileEntity> findByIdAndDeleted(final Long fileId, final boolean deletedFile, final boolean deletedPublication);
 
+	@Query(value = "SELECT * FROM FILES INNER JOIN PUBLICATIONS ON PUBLICATIONS.file_id = FILES.id WHERE FILES.id = ?1 AND FILES.deleted = ?2 AND PUBLICATIONS.deleted = ?3", nativeQuery = true)
 	public boolean existsByIdAndDeleted(final Long fileId, final boolean deletedFile, final boolean deletedPublication);
 }
