4f2e00a854359c6a53caec685654c51eb9c641a5
Wilson Barrios <wilsonbarrios@gmail.com>
2021-05-05 00:29:12 -0400

    ASO-93 Definición de las propiedades en PublicationUpdateDTO

---
+++ backend/src/main/java/com/py/aso/dto/update/PublicationUpdateDTO.java
@@ -0,0 +1,23 @@
+package com.py.aso.dto.update;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+@ApiModel(description = "Objeto que representa a una publicación, utilizado para actualizar el objeto")
+public class PublicationUpdateDTO {
+	
+	@NotBlank(message = "El contenido de la publicación es requerido")
+	@Size(min = 0, max = 255, message = "El contenido puede contener como maximo 255 caracteres")
+	@ApiModelProperty(notes = "Contenido de la publicación", example = "Contenido extenso", required = true)
+	private String body;
+	
+	@NotBlank(message = "El destinatario de la publicación es requerido")
+	@Size(min = 0, max = 10, message = "El destino puede contener como maximo 10 caracteres")
+	@ApiModelProperty(notes = "Destino de la publicación", example = "Destino", required = true)
+	private String destination;
+}
