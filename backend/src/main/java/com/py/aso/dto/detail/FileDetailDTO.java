57ccf217686cc298a42a481535d912305664776c
Wilson Barrios <wilsonbarrios@gmail.com>
2021-05-05 23:55:18 -0400

    ASO-94 Definición de las propiedades para los atributos del objeto fileDetailDTO

--- backend/src/main/java/com/py/aso/dto/detail/FileDetailDTO.java
+++ backend/src/main/java/com/py/aso/dto/detail/FileDetailDTO.java
@@ -6,7 +6,15 @@ import lombok.Data;
 @Data
 @ApiModel(description = "Objeto que representa a un archivo, se utiliza para mostrar todos los datos del objeto")
 public class FileDetailDTO {
+
+	@Positive(message = "Debe ser mayor a 0")
+	@ApiModelProperty(notes = "Numero identificador", example = "1", required = true)
 	private Long id;
+
+	@Positive(message = "Debe ser mayor a 0")
+	@ApiModelProperty(notes = "Identificador de la publicación", example = "1")
 	private Long publicationId;
+
+	@ApiModelProperty(notes = "Nombre del archivo", example = "nombreArchito.extension")
 	private String name;
 }
