22105152855bb2ba033d874d920dcaa475e6cb26
Wilson Barrios <wilsonbarrios@gmail.com>
2021-05-06 00:39:09 -0400

    ASO-95 importación de Entidad y DTOs en PublicationMapper

---
+++ backend/src/main/java/com/py/aso/service/mapper/PublicationMapper.java
@@ -0,0 +1,10 @@
+package com.py.aso.service.mapper;
+
+import org.springframework.stereotype.Component;
+
+import com.py.aso.entity.PublicationEntity;
+import com.py.aso.dto.PublicationDTO;
+import com.py.aso.dto.detail.PublicationDetailDTO;
+import com.py.aso.dto.create.PublicationCreateDTO;
+import com.py.aso.dto.update.PublicationUpdateDTO;
+
