f33a27dae5b9b9d7a3b06deb1c227b9bcdf23fc7
Wilson Barrios <wilsonbarrios@gmail.com>
2021-05-06 00:19:43 -0400

    ASO-94 Definición de las propiedades para los atributos del objeto fileUpdateDTO

--- backend/src/main/java/com/py/aso/dto/update/FileUpdateDTO.java
+++ backend/src/main/java/com/py/aso/dto/update/FileUpdateDTO.java
@@ -7,5 +7,9 @@ import lombok.Data;
 @Data
 @ApiModel(description = "Objeto que representa a un archivo, se utiliza para actualizar el objeto")
 public class FileUpdateDTO {
+
+	@NotBlank(message = "El nombre del archivo es requerido")
+	@Size(min = 0, max = 255, message = "El nombre puede contener como maximo 255 caracteres")
+	@ApiModelProperty(notes = "Nombre del archivo", example = "nombreArchito.extension", required = true)
 	private String name;
 }
