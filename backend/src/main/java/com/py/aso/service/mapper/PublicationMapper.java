70c7a4d336936b5a0ee924214149065f51238f18
Wilson Barrios <wilsonbarrios@gmail.com>
2021-05-09 00:01:44 -0400

    ASO-95 Creación de la clase y definición de los metodos

--- backend/src/main/java/com/py/aso/service/mapper/PublicationMapper.java
+++ backend/src/main/java/com/py/aso/service/mapper/PublicationMapper.java
@@ -8,3 +8,31 @@ import com.py.aso.dto.detail.PublicationDetailDTO;
 import com.py.aso.dto.create.PublicationCreateDTO;
 import com.py.aso.dto.update.PublicationUpdateDTO;
 
+@Component
+public class PublicationMapper implements BaseMapper<PublicationEntity, PublicationDTO, PublicationDetailDTO, PublicationCreateDTO, PublicationUpdateDTO> {
+
+	@Override
+	public PublicationDTO toDTO(PublicationEntity entity) {
+		return null;
+	}
+
+	@Override
+	public PublicationDetailDTO toDetailDTO(PublicationEntity entity) {
+		return null;
+	}
+
+	@Override
+	public PublicationEntity toEntity(PublicationDTO dto) {
+		return null;
+	}
+
+	@Override
+	public PublicationEntity toCreateEntity(PublicationCreateDTO dto) {
+		return null;
+	}
+
+	@Override
+	public PublicationEntity toUpdateEntity(PublicationUpdateDTO dto) {
+		return null;
+	}
+}
