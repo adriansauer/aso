22118815ada45a51b86e46d47d39d9d5687bdaa7
Wilson Barrios <wilsonbarrios@gmail.com>
2021-05-09 00:23:22 -0400

    ASO-96 implementación de la interfaz BaseMapper y definición de los metodos

--- backend/src/main/java/com/py/aso/service/mapper/FileMapper.java
+++ backend/src/main/java/com/py/aso/service/mapper/FileMapper.java
@@ -2,7 +2,42 @@ package com.py.aso.service.mapper;
 
 import org.springframework.stereotype.Component;
 
-@Component
-public class FileMapper {
+import com.py.aso.dto.FileDTO;
+import com.py.aso.dto.create.FileCreateDTO;
+import com.py.aso.dto.detail.FileDetailDTO;
+import com.py.aso.dto.update.FileUpdateDTO;
+import com.py.aso.entity.FileEntity;
 
+@Component
+public class FileMapper implements BaseMapper<FileEntity, FileDTO, FileDetailDTO, FileCreateDTO, FileUpdateDTO> {
+
+	@Override
+	public FileDTO toDTO(FileEntity entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FileDetailDTO toDetailDTO(FileEntity entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FileEntity toEntity(FileDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FileEntity toCreateEntity(FileCreateDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FileEntity toUpdateEntity(FileUpdateDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
