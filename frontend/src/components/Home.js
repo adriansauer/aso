74c26830ed39984a7bd8cfb595aa1a8d663098d8
Enzorama <enzorama013@gmail.com>
2021-05-09 16:31:39 -0400

    ASO-105 Implementacion de carta reutilizable para publicacion

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import image from '../images/construccion.png'
+import TagPublicacion from './TagsPublicaciones'
+import imagen1 from '../images/imagen1.jpg'
+import imagen2 from '../images/imagen2.jpg'
+import imagen3 from '../images/imagen3.jpg'
+import imagen4 from '../images/imagen4.jpg'
 const Home = () => {
+  const imagenes = [{ id: 1, ima: imagen1 }, { id: 2, ima: imagen2 }, { id: 3, ima: imagen3 }, { id: 4, ima: imagen4 }]
   return (
-  <div>
-        <h1 aria-setsize={36}>
-            Página de inicio
+        <div style={{ width: '100%' }}>
+            <TagPublicacion
+                name={'Nicolas Garcia'}
+                par={'Buenos dias a todos... Esta es la primera publicacion de esta red social. Es un orgullo'}
+                imagen={imagenes}
+                likes={83} />
+            <h1 aria-setsize={36}>
+                Página de inicio
        </h1>
-        <img src={image} alt="Logo" />
-    </div>)
+            <img src={image} alt="Logo" />
+        </div>)
 }
 export default Home
