c60782eb44f84b7bb9488fad009bb7ab5497d711
Wilson Barrios <wilsonbarrios@gmail.com>
2021-05-09 19:41:51 -0400

    ASO-97 implementación del método findAll

--- backend/src/main/java/com/py/aso/service/PublicationService.java
+++ backend/src/main/java/com/py/aso/service/PublicationService.java
@@ -1,5 +1,9 @@
 package com.py.aso.service;
 
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -7,18 +11,34 @@ import com.py.aso.dto.PublicationDTO;
 import com.py.aso.dto.detail.PublicationDetailDTO;
 import com.py.aso.dto.create.PublicationCreateDTO;
 import com.py.aso.dto.update.PublicationUpdateDTO;
+import com.py.aso.repository.PublicationRepository;
+import com.py.aso.service.mapper.PublicationMapper;
+
+import com.py.aso.exception.ResourceNotFoundException;
 
 public class PublicationService implements BaseService<PublicationDTO, PublicationDetailDTO, PublicationCreateDTO, PublicationUpdateDTO> {
+	
+	@Autowired
+	private PublicationRepository publicationRepository;
+	
+	@Autowired
+	private PublicationMapper publicationMapper;
 
 	@Override
+	@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
 	public Page<PublicationDTO> findAll(Pageable pageable) {
-		// TODO Auto-generated method stub
+		return this.publicationRepository.findAll(pageable)//
+				.map(this.publicationMapper::toDTO);
+	}
+	
+	@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
+	public PublicationDetailDTO findByUserId(final long id, Pageable pageable) throws Exception {
 		return null;
 	}
 
 	@Override
+	@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
 	public PublicationDetailDTO findById(long id) throws Exception {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
