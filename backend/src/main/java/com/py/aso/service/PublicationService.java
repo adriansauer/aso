e30703f604c0619879ced25e991e0151506487dc
Wilson Barrios <wilsonbarrios@gmail.com>
2021-05-09 00:59:04 -0400

    ASO-97 implementación de la interfaz BaseService y definición de los metodos

--- backend/src/main/java/com/py/aso/service/PublicationService.java
+++ backend/src/main/java/com/py/aso/service/PublicationService.java
@@ -1,5 +1,43 @@
 package com.py.aso.service;
 
-public class PublicationService {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.py.aso.dto.PublicationDTO;
+import com.py.aso.dto.detail.PublicationDetailDTO;
+import com.py.aso.dto.create.PublicationCreateDTO;
+import com.py.aso.dto.update.PublicationUpdateDTO;
+
+public class PublicationService implements BaseService<PublicationDTO, PublicationDetailDTO, PublicationCreateDTO, PublicationUpdateDTO> {
+
+	@Override
+	public Page<PublicationDTO> findAll(Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PublicationDetailDTO findById(long id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PublicationDetailDTO save(PublicationCreateDTO dto) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PublicationDetailDTO update(long id, PublicationUpdateDTO dto) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(long id) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
