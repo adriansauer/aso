1d266f7d46afe4deaf716cb2781f4d470dae1b6c
Wilson Barrios <wilsonbarrios@gmail.com>
2021-05-10 02:35:48 -0400

    ASO-103 Implementación del metodo delete

--- backend/src/main/java/com/py/aso/controller/PublicationController.java
+++ backend/src/main/java/com/py/aso/controller/PublicationController.java
@@ -3,12 +3,15 @@ package com.py.aso.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.py.aso.dto.PublicationDTO;
 import com.py.aso.dto.create.PublicationCreateDTO;
@@ -53,7 +56,12 @@ public class PublicationController implements BaseController<PublicationDTO, Pub
 	}
 
 	@Override
-	public void deleted(long id) throws Exception {
+	@DeleteMapping("/publication/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@PreAuthorize("hasRole('ROLE_SUPERUSER') or hasRole('ROLE_BRIGADE')")
+	@ApiOperation(value = "Eliminar una publicación idicada el id")
+	public void deleted(@PathVariable final long id) throws Exception {
+		this.publicationService.delete(id);
 	}
 
 }
