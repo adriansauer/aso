fa68a599b4df1dd270b96471bd387aad94203e25
Wilson Barrios <wilsonbarrios@gmail.com>
2021-05-09 21:03:26 -0400

    ASO-98 Definición de la ruta para las imagenes

--- backend/src/main/java/com/py/aso/properties/FileProperties.java
+++ backend/src/main/java/com/py/aso/properties/FileProperties.java
@@ -1,5 +1,13 @@
 package com.py.aso.properties;
 
-public class fileProperties {
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
+import lombok.Data;
+
+@Component
+@Data
+@ConfigurationProperties(prefix = "publications.files.images")
+public class fileProperties {
+	private String root;
 }
