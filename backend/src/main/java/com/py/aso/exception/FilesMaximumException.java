42f37bdd978291e8027a7b4ae37bbe6c579c6b7a
Wilson Barrios <wilsonbarrios@gmail.com>
2021-05-09 22:10:24 -0400

    ASO-99 actualice el mensaje

--- backend/src/main/java/com/py/aso/exception/FilesMaximumException.java
+++ backend/src/main/java/com/py/aso/exception/FilesMaximumException.java
@@ -2,11 +2,9 @@ package com.py.aso.exception;
 
 public class FilesMaximumException extends RuntimeException {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	public FilesMaximumException() {
-		super(String.format("Superó la cantidad máxima de archivos permitidos"));
+
+	public FilesMaximumException(final String message) {
+		super(String.format(message));
 	}
 }
