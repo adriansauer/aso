8573d186817806e292e41c5dc2b67014dc834989
Wilson Barrios <wilsonbarrios@gmail.com>
2021-05-10 01:06:27 -0400

    ASO-101 implementación del metodo save

--- backend/src/main/java/com/py/aso/service/FileService.java
+++ backend/src/main/java/com/py/aso/service/FileService.java
@@ -1,5 +1,11 @@
 package com.py.aso.service;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,11 +19,16 @@ import com.py.aso.dto.PublicationDTO;
 import com.py.aso.dto.create.FileCreateDTO;
 import com.py.aso.dto.detail.FileDetailDTO;
 import com.py.aso.dto.update.FileUpdateDTO;
+import com.py.aso.entity.FileEntity;
+import com.py.aso.entity.ImageEntity;
+import com.py.aso.exception.FileProblemsException;
 import com.py.aso.repository.FileRepository;
 import com.py.aso.service.mapper.FileMapper;
 
+import com.py.aso.properties.fileProperties;
+
 @Service
-public class FileService implements BaseService<FileDTO, FileDetailDTO, FileCreateDTO, FileUpdateDTO>{
+public class FileService<X> implements BaseService<FileDTO, FileDetailDTO, FileCreateDTO, FileUpdateDTO>{
 
 	@Autowired
 	private FileRepository fileRepository;
@@ -26,7 +37,9 @@ public class FileService implements BaseService<FileDTO, FileDetailDTO, FileCrea
 	private FileMapper fileMapper;
 	
 	@Autowired
-	private PublicationService publicationService;
+	private fileProperties fileProperties;
+	
+	private final String FILE= "file.png";
 
 	@Override
 	@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
@@ -48,18 +61,34 @@ public class FileService implements BaseService<FileDTO, FileDetailDTO, FileCrea
 	}
 
 	@Override
+	@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
 	public FileDetailDTO findById(long id) throws Exception {
-		// TODO Auto-generated method stub
+		/*
+		 * return this.fileRepository.findById(id) .map(this.fileMapper::toDetailDTO);
+		 */
 		return null;
 	}
 
 	@Override
+	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
 	public FileDetailDTO save(FileCreateDTO dto) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		final Path newFilePath = Paths.get(this.fileProperties.getRoot(), FILE).normalize();
+		final File newFile = new File(newFilePath.toUri());
+		
+		// Valida que la imagen exista.
+		final boolean isFileExists = newFile.exists();
+		if (!isFileExists) {
+			throw new FileProblemsException("El archivo no se pudo crear");
+		}
+		// Se registra en la tabla images la imagen.
+		FileEntity entity = new FileEntity();
+		entity.setName(dto.getName());
+		entity.setPath(newFilePath.toString());
+		return this.fileMapper.toDetailDTO(this.fileRepository.save(entity));
 	}
 	
-	public FileDetailDTO saveFile(final MultipartFile file, final String name) {
+	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
+	public FileDetailDTO saveFile(final String file, final String name) throws Exception {
 		return null;
 	}
 
