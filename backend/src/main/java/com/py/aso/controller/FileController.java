4c4a4aa7f6c0e434c6eb50edd5bda772b3b1d925
Wilson Barrios <wilsonbarrios@gmail.com>
2021-05-10 03:12:15 -0400

    ASO-104 Implementación del método delete

--- backend/src/main/java/com/py/aso/controller/FileController.java
+++ backend/src/main/java/com/py/aso/controller/FileController.java
@@ -5,15 +5,19 @@ import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.py.aso.dto.FileDTO;
@@ -81,9 +85,12 @@ public class FileController implements BaseController<FileDTO, FileDetailDTO, Fi
 	}
 
 	@Override
-	public void deleted(long id) throws Exception {
-		// TODO Auto-generated method stub
-		
+	@DeleteMapping("/files/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@PreAuthorize("hasRole('ROLE_SUPERUSER') or hasRole('ROLE_BRIGADE')")
+	@ApiOperation(value = "Eliminar un archivo por el id")
+	public void deleted(@PathVariable final long id) throws Exception {
+		this.fileService.delete(id);
 	}
 
 }
