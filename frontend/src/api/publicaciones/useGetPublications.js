cf452de26a18f6e67921820f1e4971f39dfca59a
adriansauer <adriansauer1999@gmail.com>
2021-05-29 09:36:44 -0400

    ASO-135 agrego un dropdown select para seleccionar el destino de la publicacion

--- frontend/src/api/publicaciones/useGetPublications.js
+++ frontend/src/api/publicaciones/useGetPublications.js
@@ -2,7 +2,7 @@ import api from '../api'
 
 const useGetPublications = () => {
   const execute = () => {
-    return api.get('api/publications')
+    return api.get('api/publications?size=10')
   }
 
   return { execute }
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,18 +1,53 @@
-import React from 'react'
-import useGetUserById from '../api/user/useGetUserById'
+import React, { useState, useEffect } from 'react'
 import Publications from './Publications'
 import InputPublicacion from './InputPublicacion'
+import useGetPublications from '../api/publicaciones/useGetPublications'
+import Loader from './PreLoader'
+import M from 'materialize-css'
 const Home = () => {
-  const user = useGetUserById(1)
-  console.log(user)
+  const { execute: getPublicationsExecute } = useGetPublications()
+  const [publicaciones, setPublicaciones] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    handleLoadPublications()
+  }, [])
+  // Reacargar publicaciones
+  const handleLoadPublications = () => {
+    setPublicaciones(null)
+    setLoading(true)
+    getPublicationsExecute()
+      .then((res) => {
+        setPublicaciones(res.data.content)
+        setLoading(false)
+      })
+      .catch((err) => {
+        setLoading(false)
+        M.toast({
+          html:
+            err.response === undefined
+              ? 'Hubo un error con la conexión'
+              : err.response.data.description
+        })
+      })
+  }
   return (
-        <div style={{ width: '100%' }}>
-            <div>
-                <InputPublicacion/>
-            </div>
-            <div>
-                <Publications/>
-            </div>
-        </div>)
+    <div style={{ width: '100%' }}>
+      <Loader visible={loading} />
+      <div>
+        {!loading
+          ? (
+          <InputPublicacion reloadPublications={handleLoadPublications} />
+            )
+          : null}
+      </div>
+      <div>
+        <Publications
+          publicaciones={publicaciones}
+          reloadPublications={handleLoadPublications}
+        />
+      </div>
+    </div>
+  )
 }
 export default Home
