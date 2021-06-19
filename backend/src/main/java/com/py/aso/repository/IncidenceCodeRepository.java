b7504e27ec514d50de86fddb86e9ea7f96d9d687
VictorCiceia <victormciceia@gmail.com>
2021-06-18 21:15:32 -0400

    ASO-177 Creacion del controlador

--- backend/src/main/java/com/py/aso/repository/IncidenceCodeRepository.java
+++ backend/src/main/java/com/py/aso/repository/IncidenceCodeRepository.java
@@ -1,5 +1,9 @@
 package com.py.aso.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +11,15 @@ import com.py.aso.entity.IncidenceCodeEntity;
 
 @Repository
 public interface IncidenceCodeRepository extends JpaRepository<IncidenceCodeEntity, Long> {
+	
+	public Page<IncidenceCodeEntity> findAllByDeleted(final boolean deleted, final Pageable pageable);
+
+	public Optional<IncidenceCodeEntity> findByIdAndDeleted(final Long id, final boolean deleted);
+	
+	public Optional<IncidenceCodeEntity> findByCodeAndDeleted(final String code, final boolean deleted);
+
+	public boolean existsByIdAndDeleted(final Long id, final boolean deleted);
+	
+	public boolean existsByCodeAndDeleted(final String code, final boolean deleted);
 
 }
